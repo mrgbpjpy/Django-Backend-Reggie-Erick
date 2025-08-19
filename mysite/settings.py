76eaaa874a84ce128108dd24f35674104d0de8ed
Erick F Esquilin <70776603+mrgbpjpy@users.noreply.github.com>
2025-08-19 17:21:37 -0400

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -28,7 +28,7 @@ DEBUG = env_bool("DJANGO_DEBUG", True)
 ALLOWED_HOSTS = env_list(
     "DJANGO_ALLOWED_HOSTS",
     [
-        "https://fastapiyoloopencv-production.up.railway.app",  # <<< UPDATE (your Railway URL)
+        "server-production-3fc4.up.railway.app",  # <<< UPDATE (your Railway URL)
         "localhost",
         "127.0.0.1",
     ],
